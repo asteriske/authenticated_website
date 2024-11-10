0c55f9703c554f2cac447e05132b0399c4b5d7e9
mccarthy <git@patrickmccarthy.cc>
2024-11-10 10:53:46 -0500

    refactor: Comment out Lambda and CloudFront distribution resources

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -20,68 +20,68 @@ resource "aws_iam_role" "lambda_edge_role" {
 }
 
 # Lambda function for authentication
-resource "aws_lambda_function" "auth_lambda" {
-  filename      = "auth_lambda.zip"
-  function_name = "auth_lambda"
-  role          = aws_iam_role.lambda_edge_role.arn
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
-  publish       = true
+# resource "aws_lambda_function" "auth_lambda" {
+#   filename      = "auth_lambda.zip"
+#   function_name = "auth_lambda"
+#   role          = aws_iam_role.lambda_edge_role.arn
+#   handler       = "index.handler"
+#   runtime       = "nodejs14.x"
+#   publish       = true
 
-  # This is a placeholder. You need to create the auth_lambda.zip file with your authentication logic
-}
+#   # This is a placeholder. You need to create the auth_lambda.zip file with your authentication logic
+# }
 
 # CloudFront distribution
-resource "aws_cloudfront_distribution" "s3_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.my_s3_bucket.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.my_s3_bucket.id}"
+# resource "aws_cloudfront_distribution" "s3_distribution" {
+#   origin {
+#     domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+#     origin_id   = "S3-${aws_s3_bucket.bucket.id}"
 
-    s3_origin_config {
-      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
-    }
-  }
+#     s3_origin_config {
+#       origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+#     }
+#   }
 
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
+#   enabled             = true
+#   is_ipv6_enabled     = true
+#   default_root_object = "index.html"
 
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.my_s3_bucket.id}"
+#   default_cache_behavior {
+#     allowed_methods  = ["GET", "HEAD"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "S3-${aws_s3_bucket.bucket.id}"
 
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
+#     forwarded_values {
+#       query_string = false
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+#     viewer_protocol_policy = "redirect-to-https"
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
 
-    lambda_function_association {
-      event_type   = "viewer-request"
-      lambda_arn   = aws_lambda_function.auth_lambda.qualified_arn
-      include_body = false
-    }
-  }
+#     lambda_function_association {
+#       event_type   = "viewer-request"
+#       lambda_arn   = aws_lambda_function.auth_lambda.qualified_arn
+#       include_body = false
+#     }
+#   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
+#   restrictions {
+#     geo_restriction {
+#       restriction_type = "none"
+#     }
+#   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-}
+#   viewer_certificate {
+#     cloudfront_default_certificate = true
+#   }
+# }
 
 # CloudFront Origin Access Identity
 resource "aws_cloudfront_origin_access_identity" "oai" {
-  comment = "OAI for ${aws_s3_bucket.my_s3_bucket.id}"
+  comment = "OAI for ${aws_s3_bucket.bucket.id}"
 }
