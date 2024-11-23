9667cbc403b0e3f9c14493352c5ece689c48b793
mccarthy <git@patrickmccarthy.cc>
2024-11-23 17:25:49 -0500

    enable outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,15 @@
 
-# # Output the Lambda function name
-# output "auth_lambda_function_name" {
-#   value = aws_lambda_function.auth_lambda.function_name
-# }
+# Output the Lambda function name
+output "auth_lambda_function_name" {
+  value = aws_lambda_function.auth_lambda.function_name
+}
 
-# # Output Cognito User Pool ID
-# output "cognito_user_pool_id" {
-#   value = aws_cognito_user_pool.main.id
-# }
+# Output Cognito User Pool ID
+output "cognito_user_pool_id" {
+  value = aws_cognito_user_pool.main.id
+}
 
-# # Output Cognito Client ID
-# output "cognito_client_id" {
-#   value = aws_cognito_user_pool_client.client.id
-# }
+# Output Cognito Client ID
+output "cognito_client_id" {
+  value = aws_cognito_user_pool_client.client.id
+}
